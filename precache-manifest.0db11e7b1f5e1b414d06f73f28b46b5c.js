948b54a1f568e559cdcb17112a6ec43f361add18
mozi47 <mozi47.shah@gmail.com>
2020-03-16 13:34:02 +0500

    Updates

---
+++ precache-manifest.0db11e7b1f5e1b414d06f73f28b46b5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d003f0455b09ea86b221fc6a0e7a88b6",
+    "url": "/React-bootstrap/index.html"
+  },
+  {
+    "revision": "415f012e4e68cbbaee66",
+    "url": "/React-bootstrap/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "27bedef2c4faf056e57e",
+    "url": "/React-bootstrap/static/css/main.5293c4c5.chunk.css"
+  },
+  {
+    "revision": "415f012e4e68cbbaee66",
+    "url": "/React-bootstrap/static/js/2.27488e90.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-bootstrap/static/js/2.27488e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bedef2c4faf056e57e",
+    "url": "/React-bootstrap/static/js/main.6714fe2a.chunk.js"
+  },
+  {
+    "revision": "525013c2e74c4873ad1b",
+    "url": "/React-bootstrap/static/js/runtime-main.ae140c07.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/React-bootstrap/static/media/logo.f391e657.svg"
+  }
+]);
